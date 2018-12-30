14829d5a1b0809875eaba38da46b25b213de9c04
Tamar Merdkhanian <tamar.merdkhanian@gmail.com>
2018-12-29 23:04:45 -0500

    added paintings page

--- src/app/paintings/paintings.component.ts
+++ src/app/paintings/paintings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-paintings',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaintingsComponent implements OnInit {
 
-  constructor() { }
+  //INSERT PAINTING NAMES TO APPEAR ON PAINTINGS PAGE
+  paintings = [{ title : "reach", dimensions : "dimensions", year : "2018", price : "100$"},
+                { title : "porto-lisbon", dimensions : "dimensions", year : "2018", price : "100$"}
+
+
+];
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  expand(item) {
+    this.router.navigate(["/paintings", item]);
   }
 
 }
